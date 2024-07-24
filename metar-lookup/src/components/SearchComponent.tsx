b1ca103407a2259c91c74ae7106bfa2dc804c022
Jubliani <59886167+Jubliani@users.noreply.github.com>
2024-07-23 22:38:51 -0700

    Added basic skeleton HTML/CSS new checkbox

--- metar-lookup/src/components/SearchComponent.tsx
+++ metar-lookup/src/components/SearchComponent.tsx
@@ -15,29 +15,46 @@ function SearchComponent( { handleClick }: SearchComponentProps ) {
     }
     return (
         <>
-            <div className="input-container">
-                <input type="text" 
-                    id="inputText" placeholder="Enter ICAO code" aria-label="Enter ICAO code" maxLength={4}
-                    value={fieldCode}
-                    onChange={e => setFieldCode(e.target.value)} />
-                <div>
-                    <button id="reqButton" type="button" onClick={onClick}>Search</button>
-                </div>
+        <div className="input-container">
+            <input type="text" 
+                id="inputText" placeholder="Enter ICAO code" aria-label="Enter ICAO code" maxLength={4}
+                value={fieldCode}
+                onChange={e => setFieldCode(e.target.value)} />
+            <div>
+                <button id="reqButton" type="button" onClick={onClick}>Search</button>
             </div>
-            <div className="checkbox-container">
-                <div id="checkboxInput">
-                    <input type="checkbox" id="TAFReq" 
-                        checked={includeTAF}
-                        onChange={e => setIncludeTAF(e.target.checked)} />
-                    <label htmlFor="TAFReq">Include TAF</label>
-                </div>
-                <div id="checkboxInput">
-                    <input type="checkbox" id="DecodeReq"
-                        checked={decode}
-                        onChange={e => setDecode(e.target.checked)} />
-                    <label htmlFor="DecodeReq">Decode Reports</label>
-                </div>
+        </div>
+        { /*skeleton code provided by https://getcssscan.com/css-checkboxes-examples */}
+        <div className="checkbox-container">
+            <div className="checkbox-wrapper-4">
+                <input className="inp-cbx" id="TAFreq" type="checkbox"
+                    checked={includeTAF}
+                    onChange={e => setIncludeTAF(e.target.checked)} />
+                <label className="cbx" htmlFor="TAFreq"><span>
+                <svg width="12px" height="10px">
+                    <use xlinkHref="#check-4"></use>
+                </svg></span><span>Include TAF</span></label>
+                <svg className="inline-svg">
+                    <symbol id="check-4" viewBox="0 0 12 10">
+                    <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                    </symbol>
+                </svg>
             </div>
+            <div className="checkbox-wrapper-4">
+                <input className="inp-cbx" id="DecodeReq" type="checkbox"
+                    checked={decode}
+                    onChange={e => setDecode(e.target.checked)} />
+                <label className="cbx" htmlFor="DecodeReq"><span>
+                <svg width="12px" height="10px">
+                    <use xlinkHref="#check-4"></use>
+                </svg></span><span>Decode reports</span></label>
+                <svg className="inline-svg">
+                    <symbol id="check-4" viewBox="0 0 12 10">
+                    <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                    </symbol>
+                </svg>
+            </div>
+        </div>
         </>
     );
 }
