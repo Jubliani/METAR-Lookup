2064dd77ea28fd5aa5b05c5e0e4b6491c4fbd176
Jubliani <59886167+Jubliani@users.noreply.github.com>
2024-07-25 15:26:35 -0700

    Added "AND" to decoding

--- metar-lookup/src/decoder/SingleWordUtils.ts
+++ metar-lookup/src/decoder/SingleWordUtils.ts
@@ -2,6 +2,7 @@ export const words: { [key: string]: string } = {
     "AO1": "Automated station without precipitation discriminator. ",
     "AO2": "Automated station with precipitation discriminator. ",
     "AUTO": "Fully automated report. ",
+    "AND": "and ",
     "DSNT": "Distant ",
     "RMK": "\n\nRemarks: \n"
 };
